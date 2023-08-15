da7b83b303451f4b0f09ee9a1e29efd191ecc568
Marcus Lundmark <marcus.lundmark@elastisys.com>
2023-08-15 13:57:40 +0200

    Verify test

--- hnc-scenario/02-wait_for_pods.sh
+++ hnc-scenario/02-wait_for_pods.sh
@@ -7,9 +7,7 @@ do
     kubectl rollout status deployment -n hnc-system hnc-controller-manager --timeout=10s > /dev/null 2>&1
     status=$?
     counter=$((counter+1))
+    sleep 3
     clear
     echo "Waiting for HNC to be running..."
-    sleep 3
 done
-
-echo "HNC is running!"
--- hnc-scenario/step1/verify.sh
+++ hnc-scenario/step1/verify.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 #Check if parent namespace company has child namespace team-abc
-kubectl hns tree company  | grep team-abc
+if kubectl hns tree company | grep team-abc ; then 
+    exit 0
+fi
