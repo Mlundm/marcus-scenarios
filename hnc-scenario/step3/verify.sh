348db6ab71271397305c1c7ffb4f9c04d246ef75
Marcus Lundmark <marcus.lundmark@elastisys.com>
2023-08-16 12:52:34 +0200

    Update last verify

--- hnc-scenario/step3/verify.sh
+++ hnc-scenario/step3/verify.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#Check if admin role exists in team-abc namespace
-output=$(kubectl get role -n team-abc)
-role="admin"
-if [[ "$output" == *"$role"* ]]; then 
-    exit 1
-fi
+#Check if admin role not exists in team-abc namespace
+! kubectl get role -n team-abc admin
